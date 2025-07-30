3632d13e34519601694b44359b3169b4c9aca542
damnis <36800988+damnis@users.noreply.github.com>
2025-07-30 15:13:10 +0200

    Update vragen_random.py

--- vragen_random.py
+++ vragen_random.py
@@ -1,7 +1,7 @@
 import random
 
-landen = ["Frankrijk", "Japan", "Canada", "Rusland", "Thailand", "België", "Spanje"]
-acties = [
+landen = ["Frankrijk", "Japan", "Canada", "Rusland", "Thailand"]
+acties_met_landen = [
     "Wie nog nooit in {} is geweest",
     "Wie ooit in {} is verdwaald",
     "Wie ooit meer dan €100 heeft uitgegeven in {}"
@@ -12,27 +12,31 @@ def genereer_random_vraag(spelers):
         return "Iedereen drinkt een slok"
 
     speler = random.choice(spelers)
-    acties = random.choice([
+    andere = random.choice([s for s in spelers if s != speler]) if len(spelers) > 1 else speler
+
+    try:
+        land_vraag = random.choice(acties_met_landen).format(random.choice(landen))
+    except:
+        land_vraag = "Iedereen die ooit op vakantie is geweest drinkt"
+
+    opties = [
         f"{speler}, vertel een gênant verhaal of drink",
-        f"{speler} en {random.choice([s for s in spelers if s != speler])} moeten tegelijk drinken",
-        random.choice(acties).format(random.choice(landen)),
-    ])
-    return actie
-
-
-
-
-
-# Simpele vraaggeneratoren
-    landen = ["Rusland", "Frankrijk", "Thailand", "Italië", "Canada", "België", "Spanje"]
-    acties = [
-        f"Wie nog nooit in {random.choice(landen)} is geweest",
-        f"{speler}, geef een compliment aan iemand of drink",
-        f"Alle spelers met een baard drinken",
+        f"{speler} en {andere} moeten tegelijk drinken",
+        land_vraag,
         f"{speler}, wijs 2 mensen aan die moeten drinken",
-        f"{speler}, vertel een gênant verhaal of drink",
-        f"Iedereen die vandaag te laat was drinkt",
-        f"De jongste in het gezelschap drinkt",
-        f"{speler} en {random.choice([n for n in st.session_state.spelers if n != speler])} drinken samen"
+        f"Wie het laatst op vakantie is geweest, drinkt",
+        f"{speler} moet iedereen een bijnaam geven (of 2 slokken nemen)"
     ]
-    vraag = random.choice(acties)
+    return random.choice(opties)
+
+
+
+
+
+
+
+
+
+
+
+# w
