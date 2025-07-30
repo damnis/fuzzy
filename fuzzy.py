f17f30d06817526f45691ec8d27c2bcd16828b52
damnis <36800988+damnis@users.noreply.github.com>
2025-07-30 22:05:42 +0200

    Update fuzzy.py

--- fuzzy.py
+++ fuzzy.py
@@ -134,7 +134,7 @@ elif st.session_state.vraag_index < len(st.session_state.vragenlijst):
 
     # Animatie effecten (vanaf vraag 2 pas)
     if random.randint(1, 3) == 1:
-        play_random_effect()
+        play_random_effect(spelers)
 
 
     if st.button("➡️ Volgende vraag"):
