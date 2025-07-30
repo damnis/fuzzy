d2916b69d32484ccae447acb2f1967413b91928c
damnis <36800988+damnis@users.noreply.github.com>
2025-07-30 15:37:58 +0200

    Update vragen_random.py

--- vragen_random.py
+++ vragen_random.py
@@ -1,10 +1,17 @@
 import random
 
+games = ["Minecraft", "FIFA", "FPS games (first person shooter)", "MMORPG (massive RPG's)", "Horror games"]
+acties_met_games = [
+    "Wie is het slechtst in {}",
+    "Wie is het best in {}",
+    "Wie heeft ooit meer dan € 200 uitgegeven aan {}"
+]
+
 landen = ["Frankrijk", "Japan", "Canada", "Rusland", "Thailand", "België", "Spanje", "Duitsland"]
 acties_met_landen = [
     "Wie nog nooit in {} is geweest",
     "Wie ooit in {} is verdwaald",
-    "Wie ooit meer dan €100 heeft uitgegeven in {}"
+    "Wie ooit meer dan € 100 heeft uitgegeven in {}"
 ]
 
 def genereer_random_vraag(spelers):
@@ -18,6 +25,12 @@ def genereer_random_vraag(spelers):
         land_vraag = random.choice(acties_met_landen).format(random.choice(landen))
     except:
         land_vraag = "Iedereen die ooit op vakantie is geweest drinkt"
+
+    try:
+        games_vraag = random.choice(acties_met_games).format(random.choice(games))
+    except:
+        games_vraag = "Wie is het meest game verslaafd en drinkt"
+        
 
     opties = [
         f"{speler}, geef de persoon links van je een kus of drink",
