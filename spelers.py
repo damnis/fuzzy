7b2352dc4841a2b0defdd07938cb47ed91ae10fe
damnis <36800988+damnis@users.noreply.github.com>
2025-07-30 14:54:05 +0200

    Update spelers.py

--- spelers.py
+++ spelers.py
@@ -1,3 +1,10 @@
+import streamlit as st
+
+def get_spelers():
+    st.subheader("👥 Spelers")
+    input_namen = st.text_input("Voer namen in, gescheiden door komma’s (bijv. Sem, Sven, Ruud)")
+    spelers = [naam.strip() for naam in input_namen.split(",") if naam.strip()]
+    return spelers
 
 
 
