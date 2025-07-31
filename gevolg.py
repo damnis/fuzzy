2b0e3f2bcc15a0e37c308fcbe70244fbf0f1ea5f
damnis <36800988+damnis@users.noreply.github.com>
2025-07-31 19:16:57 +0200

    Update gevolg.py

--- gevolg.py
+++ gevolg.py
@@ -103,7 +103,7 @@ def genereer_gevolg_vraag(spelers):
     vraag_item["andere"] = andere
 
     if vraag_item.get("type") == "uitgesteld" and "actie_knop" in vraag_item:
-    vraag_item["toon_actie_knop"] = True
+        vraag_item["toon_actie_knop"] = True
 
   
 #    vraag_item["toon_actie_knop"] = True if vraag_item.get("type") == "uitgesteld" and "actie_knop" in vraag_item else False
